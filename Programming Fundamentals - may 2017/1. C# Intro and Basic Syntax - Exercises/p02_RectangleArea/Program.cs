64a2e2b0859be1125e3f433ded376149d61e39b6
sineochk <Andzhelika93@gmail.com>
2017-06-05 09:47:07 +0300

    added exercises 2 and 3

---
+++ Programming Fundamentals - may 2017/1. C# Intro and Basic Syntax - Exercises/p02_RectangleArea/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace p02_RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double value = width * height;
+            Console.WriteLine(value.ToString("0.00", CultureInfo.InvariantCulture));
+            
+            
+
+        }
+    }
+}
