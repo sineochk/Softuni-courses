1860b9d7f14fe0dfb93ed7e0ee5a26d14e8838a2
sineochk <Andzhelika93@gmail.com>
2017-06-05 10:04:06 +0300

    added homework 2

---
+++ Programming Fundamentals - may 2017/2.C# Conditional statements and loops - Exercises/p02_ChooseDrink2.0/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace p01_ChooseDrink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string profession = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+            double prise;
+            
+            if (profession.Equals("Athlete"))
+            {
+                prise = 0.7 * quantity;
+            }
+            else if (profession.Equals("Businessman") || profession.Equals("Businesswoman"))
+            {
+                prise = 1.0 * quantity;
+            }
+            else if (profession.Equals("SoftUni Student"))
+            {
+                prise = 1.7 * quantity;
+            }
+            else
+            {
+                prise = 1.2 * quantity;
+            }
+            Console.WriteLine("The {0} has to pay {1:0.00}.", profession,prise);
+        }
+    }
+}
---
+++ Programming Fundamentals - may 2017/2.C# Conditional statements and loops - Exercises/p14_MagicLetter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace p14_MagicLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbolOne = Console.ReadLine()[0];
+            char symbolTwo = Console.ReadLine()[0];
+            char escSymbol = Console.ReadLine()[0];
+
+            for (int i = symbolOne; i <= symbolTwo; i++)
+            {
+                for (int j = symbolOne; j <= symbolTwo; j++)
+                {
+                    for (int k = symbolOne; k <= symbolTwo; k++)
+                    {
+                        if ((char)k != escSymbol && (char)j != escSymbol && (char)i != escSymbol)
+                        {
+                            Console.Write((char)i + "" + (char)j + "" + (char)k + " ");
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
