afda1f3e2b315b422a8af708b71a80c317176039
sineochk <Andzhelika93@gmail.com>
2017-06-05 09:47:55 +0300

    added exercises 4 and 5

---
+++ Programming Fundamentals - may 2017/1. C# Intro and Basic Syntax - Exercises/p05_CharacterStats/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p05_CharacterStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int currentHealth = int.Parse(Console.ReadLine());
+            int maxHealth = int.Parse(Console.ReadLine());
+            int currentEnergy = int.Parse(Console.ReadLine());
+            int maxEnergy = int.Parse(Console.ReadLine());
+
+            string currentHealthStr = new string('|', currentHealth);
+            string health = new string('.', maxHealth - currentHealth); 
+            string currentEnergyStr = new string('|', currentEnergy); 
+            string energy = new string('.', maxEnergy - currentEnergy);
+
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Health: |{0}{1}|", currentHealthStr, health );
+            Console.WriteLine("Energy: |{0}{1}|", currentEnergyStr, energy );
+
+        }
+    }
+}
